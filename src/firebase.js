f41990da51dcef19d6d29d238d5d4840857a5814
AhyoungRyu <ahyoungryu@apache.org>
2020-01-19 08:54:09 +0900

    Project initial setup with firebase conf files

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/database';
 import 'firebase/storage';
 
 const config = {
-  apiKey: "AIzaSyAfH448mv-9onnwanzxvCWo8dPt974-g8k",
-  authDomain: "react-realtime-chatting-app.firebaseapp.com",
-  databaseURL: "https://react-realtime-chatting-app.firebaseio.com",
-  projectId: "react-realtime-chatting-app",
-  storageBucket: "react-realtime-chatting-app.appspot.com",
-  messagingSenderId: "1076388803614",
-  appId: "1:1076388803614:web:74118d36202c118744ea0f",
-  measurementId: "G-L5FMYGXKQR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 firebase.initializeApp(config);
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,75 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {
+  Provider,
+  useDispatch,
+ } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from "./dux/rootReducer";
+
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+import Spinner from './components/reusable/UI/Spinner';
 
 import registerServiceWorker from './registerServiceWorker';
 
+import {
+  setUser,
+  clearUser,
+ } from './dux/user/actions';
+
 import 'semantic-ui-css/semantic.min.css';
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter,
+ } from "react-router-dom";
 
-const Root = () => (
-  <Router>
+import firebase from './firebase';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+const Root = ({ history }) => {
+  const dispatch = useDispatch();
+  const [user, loading] = useAuthState(firebase.auth());
+
+  useEffect(() => {
+    if (!loading) {
+      if (user) {
+        dispatch(setUser({ 
+          id: user.uid,
+          name: user.displayName,
+          avatar: user.photoURL,
+        }));
+        history.push('/');
+      } else {
+        history.push('/login');
+        dispatch(clearUser());
+      }
+    }
+  }, [user, loading]);
+  if (loading) {
+    return <Spinner content="Loading chat..." />;
+  }
+  return (
     <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
     </Switch>
-  </Router>
-)
-ReactDOM.render(<Root />, document.getElementById('root'));
+  )
+}
+
+const RootWithAuth = withRouter(Root);
+ReactDOM.render(
+  <Provider store={store}>
+    <Router><RootWithAuth /></Router>
+  </Provider>
+  , document.getElementById('root')
+);
 registerServiceWorker();
